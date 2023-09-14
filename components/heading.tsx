a5247199be14a92aa1d35590a36cd236a40aaf49
Dogukanerzurum06 <93072322+Dogukanerzurum06@users.noreply.github.com>
2023-09-14 14:50:48 +0300

    The infrastructure of artificial intelligence for the Conversation part has started to be created. (Not completed)

--- components/heading.tsx
+++ components/heading.tsx
@@ -18,7 +18,6 @@ export const Heading = ({
     bgColor
 }: HeadingProps) => {
   return (
-    <>
     <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
       <div className={cn("p-2 w-fit rounded-md", bgColor)}>
         <Icon className={cn("w-10 h-10", iconColor)}/>
@@ -27,9 +26,10 @@ export const Heading = ({
         <h2 className="text-3xl font-bold">
           {title}
         </h2>
+        <p className="text-sm text-muted-foreground">
+          {description}
+        </p>
       </div>
     </div>
-    
-    </>
   );
 };
